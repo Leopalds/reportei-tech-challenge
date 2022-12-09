d55c0ebb31bcffeea2ac49399f1a77f0735a0b31
Leopalds <pedro.leopoldo@estudante.ufjf.br>
2022-12-08 22:08:30 -0300

    Starting project with Github auth

---
+++ reportei-tech-challenge/app/Http/Controllers/Auth/GithubAuthController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Models\User;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+
+class GithubAuthController extends Controller
+{
+    public function callback()
+    {
+        $githubUser = Socialite::driver('github')->user();
+
+        $user = User::updateOrCreate([
+            'github_id' => $githubUser->id,
+        ],[
+            'name' => $githubUser->name,
+            'email' => $githubUser->email,
+            'github_token' => $githubUser->token,
+            'github_refresh_token' => $githubUser->refreshToken,
+        ]);
+
+        Auth::login($user);
+        
+        return redirect('/dashboard');
+    }
+}
---
+++ reportei-tech-challenge/routes/web.php
@@ -0,0 +1,32 @@
+<?php
+
+use App\Http\Controllers\auth\GithubAuthController;
+use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+Route::get('/auth/redirect', function (){
+    return Socialite::driver('github')->redirect();
+})->name('auth.github');
+
+Route::get('/auth/callback', [GithubAuthController::class, 'callback'])->name('auth.callback');
+
+require __DIR__.'/auth.php';
